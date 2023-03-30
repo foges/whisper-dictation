7e1324b205cdb9a39e943a8d5b038be0e61d8ad0
Chris Fougner <chris.fougner@gmail.com>
2023-03-30 21:24:58 +0200

    better args parse

--- whisper-dictation.py
+++ whisper-dictation.py
@@ -9,13 +9,12 @@ from whisper import load_model
 import platform
 
 class SpeechTranscriber:
-    def __init__(self, model, language=None):
+    def __init__(self, model):
         self.model = model
-        self.language = language
         self.pykeyboard = keyboard.Controller()
 
-    def transcribe(self, audio_data):
-        result = self.model.transcribe(audio_data, language=self.language)
+    def transcribe(self, audio_data, language=None):
+        result = self.model.transcribe(audio_data, language=language)
         is_first = True
         for element in result["text"]:
             if is_first and element == " ":
@@ -33,25 +32,27 @@ class Recorder:
         self.recording = False
         self.transcriber = transcriber
 
-    def start(self):
-        thread = threading.Thread(target=self._record_impl)
+    def start(self, language=None):
+        thread = threading.Thread(target=self._record_impl, args=(language,))
         thread.start()
 
     def stop(self):
         self.recording = False
 
-    def _record_impl(self):
+
+    def _record_impl(self, language):
         self.recording = True
-        chunk = 1024
-        sample_format = pyaudio.paInt16
-        channels = 1
-        fs = 16000
+        frames_per_buffer = 1024
         p = pyaudio.PyAudio()
-        stream = p.open(format=sample_format, channels=channels, rate=fs, frames_per_buffer=chunk, input=True)
+        stream = p.open(format=pyaudio.paInt16,
+                        channels=1,
+                        rate=16000,
+                        frames_per_buffer=frames_per_buffer,
+                        input=True)
         frames = []
 
         while self.recording:
-            data = stream.read(chunk)
+            data = stream.read(frames_per_buffer)
             frames.append(data)
 
         stream.stop_stream()
@@ -60,7 +61,7 @@ class Recorder:
 
         audio_data = np.frombuffer(b''.join(frames), dtype=np.int16)
         audio_data_fp32 = audio_data.astype(np.float32) / 32768.0
-        self.transcriber.transcribe(audio_data_fp32)
+        self.transcriber.transcribe(audio_data_fp32, language)
 
 
 class GlobalKeyListener:
@@ -93,22 +94,44 @@ class GlobalKeyListener:
 
 
 class StatusBarApp(rumps.App):
-    def __init__(self, recorder, max_time=None):
+    def __init__(self, recorder, languages=None, max_time=None):
         super().__init__("whisper", "⏯")
-        self.menu = ['Start', 'Stop']
+        self.languages = languages
+        self.current_language = languages[0] if languages is not None else None
+
+        menu = [
+            'Start Recording',
+            'Stop Recording',
+            None,
+        ]
+
+        if languages is not None:
+            for lang in languages:
+                callback = self.change_language if lang != self.current_language else None
+                menu.append(rumps.MenuItem(lang, callback=callback))
+            menu.append(None)
+            
+        self.menu = menu
+        self.menu['Stop Recording'].set_callback(None)
+
         self.started = False
         self.recorder = recorder
         self.max_time = max_time
         self.timer = None
         self.elapsed_time = 0
 
-    @rumps.clicked('Start')
+    def change_language(self, sender):
+        self.current_language = sender.title
+        for lang in self.languages:
+            self.menu[lang].set_callback(self.change_language if lang != self.current_language else None)
+
+    @rumps.clicked('Start Recording')
     def start_app(self, _):
         print('Listening...')
         self.started = True
-        self.menu['Start'].set_callback(None)
-        self.menu['Stop'].set_callback(self.stop_app)
-        self.recorder.start()
+        self.menu['Start Recording'].set_callback(None)
+        self.menu['Stop Recording'].set_callback(self.stop_app)
+        self.recorder.start(self.current_language)
 
         if self.max_time is not None:
             self.timer = threading.Timer(self.max_time, lambda: self.stop_app(None))
@@ -117,16 +140,19 @@ class StatusBarApp(rumps.App):
         self.start_time = time.time()
         self.update_title()
 
-    @rumps.clicked('Stop')
+    @rumps.clicked('Stop Recording')
     def stop_app(self, _):
+        if not self.started:
+            return
+        
         if self.timer is not None:
             self.timer.cancel()
 
         print('Transcribing...')
         self.title = "⏯"
         self.started = False
-        self.menu['Stop'].set_callback(None)
-        self.menu['Start'].set_callback(self.start_app)
+        self.menu['Stop Recording'].set_callback(None)
+        self.menu['Start Recording'].set_callback(self.start_app)
         self.recorder.stop()
         print('Done.\n')
 
@@ -144,7 +170,7 @@ class StatusBarApp(rumps.App):
             self.start_app(None)
 
 
-if __name__ == "__main__":
+def parse_args():
     parser = argparse.ArgumentParser(
         description='Dictation app using the OpenAI whisper ASR model. By default the keyboard shortcut cmd+option '
         'starts and stops dictation')
@@ -157,8 +183,8 @@ if __name__ == "__main__":
                         'Note that the models ending in .en are trained only on English speech and will perform better on English '
                         'language. Note that the small, medium, and large models may be slow to transcribe and are only recommended '
                         'if you find the base model to be insufficient. Default: base.')
-    parser.add_argument('-k', '--key_combination', type=str, default='cmd_r+alt' if platform.system() == 'Darwin' else 'ctrl+alt',
-                        help='Specify the key combination to toggle the app. Example: cmd_r+alt for macOS '
+    parser.add_argument('-k', '--key_combination', type=str, default='cmd_l+alt' if platform.system() == 'Darwin' else 'ctrl+alt',
+                        help='Specify the key combination to toggle the app. Example: cmd_l+alt for macOS '
                         'ctrl+alt for other platforms. Default: cmd_r+alt (macOS) or ctrl+alt (others).')
     parser.add_argument('-l', '--language', type=str, default=None,
                         help='Specify the two-letter language code (e.g., "en" for English) to improve recognition accuracy. '
@@ -168,18 +194,29 @@ if __name__ == "__main__":
                         help='Specify the maximum recording time in seconds. The app will automatically stop recording after this duration. '
                         'Default: 30 seconds.')
 
-
-
     args = parser.parse_args()
+
+    if args.language is not None:
+        args.language = args.language.split(',')
+
+    if args.model_name.endswith('.en') and args.language is not None and any(lang != 'en' for lang in args.language):
+        raise ValueError('If using a model ending in .en, you cannot specify a language other than English.')
+
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
 
     print("Loading model...")
     model_name = args.model_name
     model = load_model(model_name)
     print(f"{model_name} model loaded")
-    transcriber = SpeechTranscriber(model, args.language)
+    
+    transcriber = SpeechTranscriber(model)
     recorder = Recorder(transcriber)
     
-    app = StatusBarApp(recorder, args.max_time)
+    app = StatusBarApp(recorder, args.language, args.max_time)
     key_listener = GlobalKeyListener(app, args.key_combination)
     listener = keyboard.Listener(on_press=key_listener.on_key_press, on_release=key_listener.on_key_release)
     listener.start()
